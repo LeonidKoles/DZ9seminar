7dd07748e5996acc32f4564e0e89ff0b73867c9d
Колесниченко Леонид <Leon-kol1994@rambler.ru>
2022-08-25 21:46:37 +0300

    zadacha68

---
+++ Zadacha68/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+int Zadacha68(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > n && n == 0) return Zadacha68(m - 1, 1);
+    if (m > n && n > 0) return Zadacha68(m - 1, Zadacha68(m, n - 1));
+    return n + 1;
+    Console.WriteLine(Zadacha68(m, n));
+    Zadacha68(m,n);
+
+
+}
+Zadacha68(1, 2);
