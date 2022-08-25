380a19d9d239a6ad6961e051787faa5f062134f2
Колесниченко Леонид <Leon-kol1994@rambler.ru>
2022-08-25 21:46:16 +0300

    zadacha66

---
+++ Zadacha66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите рекурсионный метод, 
+// который найдёт сумму натуральных элементов в промежутке от M до N.
+
+void Zadacha66(int m, int n, int sum = 0)
+{
+    if (m == n + 1)
+    {
+        Console.WriteLine(sum);
+        return;
+    }
+
+    sum += m;
+    m++;
+    Zadacha66(m, n, sum);
+}
+Zadacha66(1, 15);
+
